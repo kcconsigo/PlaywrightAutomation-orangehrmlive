0e7b6af1f0b918fb33442bfd76c3a9187b9076b3
kcconsigo <kcconsigo@gmail.com>
2024-10-21 14:00:59 +0800

    Refactor config.js

--- playwright.config.js
+++ playwright.config.js
@@ -34,9 +34,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["line"], ["allure-playwright"],["json", {
-    outputFile: "jsonReports/jsonReport.json"
-  }]],
+  reporter: [["line"], ["allure-playwright"]],
   // reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
