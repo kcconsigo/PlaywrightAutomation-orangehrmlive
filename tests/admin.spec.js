f5b2f402dd63aa7b7f6d293b7c921d17f7cf700d
kcconsigo <kcconsigo@gmail.com>
2024-10-23 18:20:51 +0800

    Refactored admin, editadmin and its testdata to update

--- tests/admin.spec.js
+++ tests/admin.spec.js
@@ -4,26 +4,28 @@ const { LoginPage } = require('../pages/loginpage.page');
 const dataSetCreateAdmin = JSON.parse(JSON.stringify(require("../utils/adminTestData.json")));
 const dataSetRoleAndStatus = JSON.parse(JSON.stringify(require("../utils/adminroleandstatusTestData.json")));
 
-for(const createdata of dataSetCreateAdmin)
-{
-        test(`Create Admin ${createdata.EmpName},${createdata.UserName},${createdata.Password},${createdata.ConfirmPassword}`, async ({ page })=> {
+test.describe('User Creates Admin ', async () => {
+  for(const createdata of dataSetCreateAdmin)
+       {
+          test(`Create Admin ${createdata.EmpName},${createdata.UserName},${createdata.Password},${createdata.ConfirmPassword}`, async ({ page })=> {
+                
+             const loginpage = new LoginPage(page);
+             await page.waitForTimeout(2000);
+             await loginpage.gotoLoginPage()
+             await loginpage.loginCreds(createdata.username,createdata.password)
+             await loginpage.clickLoginBtn()
+             await page.waitForTimeout(5000);
+                        
+             const adminpage = new AdminPage(page);
+             await adminpage.AdminTab();
+             await adminpage.selectUserRoleAndUserStatus();
+             await adminpage.createNewEmployeeInputTextFields(createdata.EmpName, createdata.UserName,createdata.Password,createdata.ConfirmPassword);
+             await adminpage.clickSavebtn();
+                        
+            })
+      }
+});
 
-                const loginpage = new LoginPage(page);
-                await page.waitForTimeout(2000);
-                await loginpage.gotoLoginPage()
-                await loginpage.loginCreds(createdata.username,createdata.password)
-                await loginpage.clickLoginBtn()
-                await page.waitForTimeout(5000);
-        
-                const adminpage = new AdminPage(page);
-                await adminpage.AdminTab();
-                await adminpage.selectUserRoleAndUserStatus();
-                await adminpage.createNewEmployeeInputTextFields(createdata.EmpName, createdata.UserName,createdata.Password,createdata.ConfirmPassword);
-                await adminpage.clickSavebtn();
-                await adminpage.verifyAdmin(createdata.UserName)
-        
-        })
-}
 
        
 
--- tests/editadmin.spec.js
+++ tests/editadmin.spec.js
@@ -4,10 +4,10 @@ const { LoginPage } = require('../pages/loginpage.page');
 
 const dataSetEditDeleteAdmin = JSON.parse(JSON.stringify(require("../utils/editadmininfoTestData.json")));
 
-test.describe('Navigates to Admin dashboard', async () => {
+test.describe('User configures Admin Information ', async () => {
     for(const dataEditAdmin of dataSetEditDeleteAdmin)
         {
-            test(`should allow to Edit and Delete Admin ${createdata.EditEmpName},${dataEditAdmin.firstName},${dataEditAdmin.lastName},${dataEditAdmin.lastName},${dataEditAdmin.empID}`, {tag: '@EndtoEndTesting'}, async ({ page })=> {
+        test(`should allow to Edit and Delete Admin ${dataEditAdmin.EditEmpName},${dataEditAdmin.EditUserName},${dataEditAdmin.EditPassword},${dataEditAdmin.EditConfirmPassword}`, {tag: '@EndtoEndTesting'}, async ({ page })=> {
 
         const editadmininfo = new EditAdminPage(page);
         await editadmininfo.adminpageDashboard();
