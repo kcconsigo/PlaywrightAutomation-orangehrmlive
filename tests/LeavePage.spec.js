dc6eef255f9f3060c5682ebc08fa2484ca8cec1c
kcconsigo <kcconsigo@gmail.com>
2024-11-22 00:23:03 +0800

    [tests/LeavePage.spec.js] Add LeavePage tst

---
+++ tests/LeavePage.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../pages/loginpage.page');
+const { LeavePage } = require('../pages/leavepage.page');
+
+const dataLeave = JSON.parse(JSON.stringify(require("../utils/leaveTestData.json")));
+
+
+test('Should allow an Employee to have leave entitlements', async({ page }) => {
+
+        const loginpage = new LoginPage(page);
+        await page.waitForTimeout(2000);
+        await loginpage.gotoLoginPage();
+        await loginpage.loginCreds(dataLeave.username, dataLeave.password);
+        await loginpage.clickLoginBtn();
+        await page.waitForTimeout(3000);
+
+        const leavepage = new LeavePage(page);
+        await page.waitForTimeout(2000);
+        await leavepage.leaveTab();
+        await page.waitForTimeout(3000);
+        await leavepage.entitlementTab(dataLeave.firstName);
+})
