96d079ed552da1867574530e95137aa70986a044
kcconsigo <kcconsigo@gmail.com>
2025-01-22 15:23:55 +0800

    Modified these files to be commit = > config/cucumber.js package.json

---
+++ config/cucumber.js
@@ -0,0 +1,4 @@
+module.exports = { default: '--publish-quite' }
+// npx cucumber-js --tags "@E2Etesting" --exit --format html:cucumber-report.html
+
+// npx cucumber-js features/Login.feature --tags "@E2Etesting" --exit --format html:cucumber-report.html
